bbc5e7d4a3ebe9f658d12f1fb7b6352b4a00952c
Robin <me@taminoful.de>
2015-11-05 16:22:39 +0100

    Added 'src'-Folder.
    .

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.awt.Toolkit;
+import java.net.InetAddress;
+
+public class Main {
+	
+	public static int attempts = 1;
+	
+	public static void main(String[] args){
+		ping("127.0.0.1"); // Insert your IP or Hostname.
+    }
+	
+	public static void ping(String ip) {
+		try {
+            InetAddress address = InetAddress.getByName(ip);
+            boolean reachable = address.isReachable(10000);
+
+            int index = 0;
+            while(index == 0) {
+            	if(reachable == true) {
+            		System.out.println("Attempt " + attempts + ": Successfully connected to '" + ip + "'.");
+                	Toolkit.getDefaultToolkit().beep();
+                	break;
+                } else if (reachable == false) {
+                	System.out.println("Attempt " + attempts + ": Connection to '" + ip + "' failed.");
+                	attempts++;
+                }            	
+            	Thread.sleep(2500);
+                index++;
+                ping(ip);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+	}
+}
